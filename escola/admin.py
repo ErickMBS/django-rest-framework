459697975b2f7efcf3aaa6e8e4100da8f2a9e3c1
erick_braga <@Erickmatheus07>
2022-01-18 23:37:13 -0300

    Finalizada aula 2

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+
+class Alunos(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nome',
+        'rg',
+        'cpf',
+        'data_nascimento'
+    )
+    list_display_links = ('id', 'nome',)
+    search_fields = ('id', 'nome', 'rg', 'cpf')
+    list_per_page = 20
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'codigo_curso',
+        'descricao',
+        'nivel'
+    )
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('id', 'codigo_curso')
+    list_filter = ('nivel',)
+
+
+admin.site.register(Aluno, Alunos)
+admin.site.register(Curso, Cursos)
+
+
